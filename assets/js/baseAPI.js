04399f6779bb52d49b7525d81fd6cba67522cb4d
zcj <3336457562@qq.com>
2022-04-19 14:46:12 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,23 @@ $.ajaxPrefilter(function (options) {
     // 在发起真正的Ajax请求时，统一拼接接口的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
     // console.log(options.url)
+
+    // 统一为有权限的接口，配置header对象
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete函数,防止每次访问都要挂载
+    options.complete = function (res) {
+        console.log(res)
+        // responseJSON可以得到后台响应回来的数据
+        if (res.responseJSON.status === 1 || res.responseJSON.message === '身份认证失败！') {
+            // 清空token
+            localStorage.removeItem('token')
+            // 跳转到登录页
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+$(function () {
+    getUserInfo()
+    // console.log('cg')
+    const layer = layui.layer
+    $('#btnLogOut').click(function () {
+        // 弹出询问框
+        layer.confirm('确认退出登录吗?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1、清除token
+            localStorage.removeItem('token')
+            //2、 跳转到登录页面
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //属性字母不能大写
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            // console.log(res)
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        },
+        // 控制用户的访问权限 ,如果不控制，在输入主页url时可直接访问
+        // 不论成功还是失败，最终都会调用 complete 回调函数
+        // complete: function (res) {
+        //     console.log(res)
+        //     // responseJSON可以得到后台响应回来的数据
+        //     if (res.responseJSON.status === 1 || res.responseJSON.message === '身份认证失败！') {
+        //         // 清空token
+        //         localStorage.removeItem('token')
+        //         // 跳转到登录页
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+// 渲染用户头像
+function renderAvatar(user) {
+    // 1、设置用户昵称
+    const name = user.username || user.nickname
+    // 2、设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    //3、 按需渲染头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('text-avatar').hide()
+    }
+    else {
+        $('.layui-nav-img').hide()
+        const first = name[0].toUpperCase()
+        $('text-avatar').html(first).show()
+    }
+}
